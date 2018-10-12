29a45ff27b39525d29c245e905140602d404189a
Alexander Quintanilla <32335891+XanderTheGOAT@users.noreply.github.com>
2018-10-12 14:28:57 -0600

    Folder Additions

--- Pente/Pente/View/MainWindow.xaml.cs
+++ Pente/Pente/View/MainWindow.xaml.cs
@@ -24,5 +24,8 @@ namespace Pente
         {
             InitializeComponent();
         }
+
+
+
     }
 }
---
+++ Pente/PenteDLL/Controllers/Pente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pente.Controllers
+{
+    class Pente
+    {
+        public void RestartGame()
+        {
+
+        }
+
+        public void Tessera()
+        {
+
+        }
+
+        public void Tria()
+        {
+
+        }
+    }
+}
