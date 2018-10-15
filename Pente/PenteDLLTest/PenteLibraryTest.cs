1a0a2b99e870f280a9115eb61874c2dc062cbaed
CuddlySpartan <ftruso99@gmail.com>
2018-10-15 14:42:54 -0600

    Stubbed out test methods

---
+++ Pente/PenteDLLTest/PenteLibraryTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PenteDLLTest
+{
+    [TestClass]
+    public class PenteLibraryTest
+    {
+        [TestMethod]
+        public void TesseraTestVerticle()
+        {//this way -> |
+
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        public void TesseraTestHorizontal()
+        {//this way -> -
+
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        public void TesseraTestDiagonalPositive()
+        {// this way -> /
+
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        public void TesseraTestDiagonalNegative()
+        {// this way -> \
+
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void TriaTestVerticle()
+        {//this way -> |
+
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        public void TriaTestHorizontal()
+        {//this way -> -
+
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        public void TriaTestDiagonalPositive()
+        {// this way -> /
+
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        public void TriaTestDiagonalNegative()
+        {//this way -> \
+
+            Assert.Inconclusive();
+        }
+    }
+}
