7d5d8fdeee61ea5808036230073b8ed1d20e365c
CuddlySpartan <ftruso99@gmail.com>
2018-10-15 14:21:25 -0600

    Added Unit Test Project

---
+++ Pente/PenteDLLTest/PenteTeste.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PenteDLLTest
+{
+    [TestClass]
+    public class PenteTeste
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
