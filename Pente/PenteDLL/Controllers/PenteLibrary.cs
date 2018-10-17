e7b3277085c10ffeacacd160e646704622863f17
Alexander Quintanilla <32335891+XanderTheGOAT@users.noreply.github.com>
2018-10-17 13:34:47 -0600

    Movement updateMethods

--- Pente/PenteDLL/Controllers/PenteLibrary.cs
+++ Pente/PenteDLL/Controllers/PenteLibrary.cs
@@ -42,45 +42,55 @@ namespace PenteDLL.Controllers
         public bool FiveInARow()
         {
 
+            switch()
+            case UpDown:
 
+            case LeftRight:
 
+            case UpRightDownRight:
+
+            case UpLeftDownLeft:
+
+            default:
 
             return false;
         }
 
-        //public bool FiveUp(int i, int j, int a)
-        //{
-        //    if (a == 4)
-        //    {
-        //        return true;
-        //    }
+        public int CheckUp(int i, int j, int a)
+        {
+            if (a == 4)
+            {
+                return true;
+            }
 
-        //    if ((i - 1) >= 0 && Board[i, j] == Board[i-1, j])
-        //    {
-        //        if (FiveUp(i - 1, j, a + 1))
-        //        {
-        //            return true;
-        //        }
-        //        else
-        //        {
-        //            return false;
-        //        }
-        //    }
-        //}
+            if ((i - 1) >= 0 && Board[i, j] == Board[i - 1, j])
+            {
+                if (CheckUp(i - 1, j, a + 1))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
 
-        //public bool FiveDown(int i, int j)
-        //{
+        public int CheckDown(int i, int j)
+        {
+            return null;
 
-        //}
+        }
 
-        //public bool FiveLeft(int i, int j)
-        //{
+        public int CheckLeft(int i, int j)
+        {
+            return null;
 
-        //}
+        }
 
-        //public bool FiveRight(int i, int j)
-        //{
-
-        //}
+        public int CheckRight(int i, int j)
+        {
+            return null;
+        }
     }
 }
